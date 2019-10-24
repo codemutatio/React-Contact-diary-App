5df2f68edef94b5749a1837530448fa7c3ae754a
codemutatio <faithomojola@gmail.com>
2019-10-24 08:14:06 +0100

    version 1 upload

---
+++ build/precache-manifest.c3fab32dc668c10abd4d049f4121d474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d4b2b90a9eef3cf01245be98d36c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd68b96789b3e6bfe19a",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "ebee702f49e67f40a714",
+    "url": "/static/css/main.64eac7d8.chunk.css"
+  },
+  {
+    "revision": "dd68b96789b3e6bfe19a",
+    "url": "/static/js/2.099d2123.chunk.js"
+  },
+  {
+    "revision": "ebee702f49e67f40a714",
+    "url": "/static/js/main.6a3cbac3.chunk.js"
+  },
+  {
+    "revision": "3526cfc7e56b2bf0fe53",
+    "url": "/static/js/runtime-main.0f8ab66f.js"
+  }
+]);
